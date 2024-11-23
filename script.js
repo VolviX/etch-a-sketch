d408fd5319e810f0e3757387b7681202e5de4d9d
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-23 18:37:39 +0300

    Add grid with appropriate name

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function board() {
-    let main = document.querySelector('.main');
+    let main = document.querySelector('.container');
     let row = [];
     let column = [];
 
     for (let i = 0; i < 16; i++) {
         row[i] = document.createElement('div');
+        row[i].className = `row${i}`;
         main.appendChild(row[i]);
         console.log(`Row created: ${i}`);
 
         for (let j = 0; j < 16; j++) {
             column[j] = document.createElement('div');
+            column[j].className = `column${j}`;
             row[i].appendChild(column[j]);
             console.log(`Column created: ${j}`);
         }
