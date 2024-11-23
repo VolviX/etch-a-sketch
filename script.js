d9e7da18309ac2aba1bc4847f706c4601569d3cc
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-23 22:59:00 +0300

    Add button to change canvas size

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function board() {
+function board(max) {
     main.replaceChildren(); // Clear previous rows and cells
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < max; i++) {
         const row = document.createElement('div');
         main.appendChild(row);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < max; j++) {
             const column = document.createElement('div');
             column.className = 'cell';
             row.appendChild(column);
@@ -20,6 +20,18 @@ main.addEventListener('mouseover', (event) => {
 });
 
 const resize = document.querySelector('.resize');
-resize.addEventListener('click', board);
+resize.addEventListener('click', () => {
+
+    while (true) {
+        const userInput = prompt('Enter the desired canvas size between 1 and 100: ');
+
+        if (userInput >= 1 && userInput <= 100) {
+            board(userInput);
+            break;
+        } else {
+            alert('The input must be between 1 and 100');
+        }
+    }
+});
 
 board();
