63a5ef45958d8bd64e5a027200d1b57849e46154
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-23 18:42:29 +0300

    Refactor the code

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ function board() {
     let row = [];
     let column = [];
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 1; i < 17; i++) {
         row[i] = document.createElement('div');
-        row[i].className = `row${i}`;
         main.appendChild(row[i]);
-        console.log(`Row created: ${i}`);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 1; j < 17; j++) {
             column[j] = document.createElement('div');
-            column[j].className = `column${j}`;
+            column[j].className = `cell${i}-${j}`;
             row[i].appendChild(column[j]);
-            console.log(`Column created: ${j}`);
         }
     }
 }
