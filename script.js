18248b240b71835df182d0d1587126d536b566f8
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-23 21:12:15 +0300

    Add button to clear canvas

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function board() {
-    const row = [];
-    const column = [];
-
+    main.replaceChildren(); // Clear previous rows and cells
     for (let i = 0; i < 16; i++) {
-        row[i] = document.createElement('div');
-        main.appendChild(row[i]);
+        const row = document.createElement('div');
+        main.appendChild(row);
 
         for (let j = 0; j < 16; j++) {
-            column[j] = document.createElement('div');
-            column[j].className = 'cell';
-            row[i].appendChild(column[j]);
+            const column = document.createElement('div');
+            column.className = 'cell';
+            row.appendChild(column);
         }
     }
 }
 
 const main = document.querySelector('.container');
-
 main.addEventListener('mouseover', (event) => {
     if (event.target.classList.contains('cell')) {
         event.target.style.backgroundColor = 'pink';
     }
 });
+
+const resize = document.querySelector('.resize');
+resize.addEventListener('click', board);
 
 board();
