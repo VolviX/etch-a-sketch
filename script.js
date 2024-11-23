551402d26e3d2d13fbc4067dc273291ea56c4ec8
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-23 19:39:44 +0300

    Add mouse over color trail

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function board() {
-    let main = document.querySelector('.container');
-    let row = [];
-    let column = [];
+    const row = [];
+    const column = [];
 
-    for (let i = 1; i < 17; i++) {
+    for (let i = 0; i < 16; i++) {
         row[i] = document.createElement('div');
         main.appendChild(row[i]);
 
-        for (let j = 1; j < 17; j++) {
+        for (let j = 0; j < 16; j++) {
             column[j] = document.createElement('div');
-            column[j].className = `cell${i}-${j}`;
+            column[j].className = 'cell';
             row[i].appendChild(column[j]);
         }
     }
 }
+
+const main = document.querySelector('.container');
+
+main.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('cell')) {
+        event.target.style.backgroundColor = 'pink';
+    }
+});
 
 board();
